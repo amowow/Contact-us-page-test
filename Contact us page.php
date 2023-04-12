6b7788d1d807c3ab4771854bc8be4640a884f0f3
amowow <127686917+amowow@users.noreply.github.com>
2023-04-12 23:13:42 +0300

    Update Contact us page.php

--- Contact us page.php
+++ Contact us page.php
@@ -1,19 +1,47 @@
 <?php
-//geting the date from the form
-$Name = $_POST['Name'];
-$Email = $_POST['Email'];
-$Message = $_POST['Message'];
 
-$to = "ameen100941@outlook.com";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Get the form fields and remove whitespace
+  $name = trim($_POST["name"]);
+  $email = trim($_POST["email"]);
+  $message = trim($_POST["message"]);
 
-$subject ="MovieT contact"
-$txt ="Name =" . $Name . "\r\n Email = " . $Email . "\r\n Message =" . $Message;
+  // Check for empty fields
+  if (empty($name) || empty($email) || empty($message)) {
+    echo "Please fill in all the fields.";
+    exit;
+  }
 
-$headers = "From: MovieT Contact" . "\r\n" . "CC: ameen100942@outlook.com";
-if($Email !=null){
-    mail($to,$subject,$txt,$headers);
+  // Validate the email address
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "Please enter a valid email address.";
+    exit;
+  }
+
+  // Set the recipient email address
+  $to = "ameen100941@outlook.com";
+
+  // Set the email subject
+  $subject = "New Contact Form Submission";
+
+  // Build the email body
+  $body = "Name: $name\n\n";
+  $body .= "Email: $email\n\n";
+  $body .= "Message: $message\n\n";
+
+  // Set the email headers
+  $headers = "From: $email\r\n";
+  $headers .= "Reply-To: $email\r\n";
+
+  // Send the email
+  if (mail($to, $subject, $body, $headers)) {
+    // Redirect to the thank you page
+    header("Location: Thank_you page.html");
+    exit;
+  } else {
+    echo "Oops! Something went wrong and we couldn't send your message.";
+    exit;
+  }
 }
-// getting them to thank you page
 
-$header("Location: Thank_you page.html")
 ?>
